1f323d1024486d0461bd5bce61fc74f0287814e3
Your Name <you@example.com>
2020-08-20 11:38:30 -0300

    'ddws

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ io.on('connection', socket =>{
     })
 
     socket.on('enviajson',function(data){
-        console.log('rom open' + data.nomeproduto)
+        //console.log('rom open' + data.nomeproduto)
         //let json = JSON.parse(data)
+        Math.round(x) 
+
+        let valorunitario = parseFloat(data.valorunitario).toFixed(2)
+        data.valorunitario = valorunitario
         
        io.sockets.in(data.room).emit('receive', data);
    })
